e8f4d0dd4e42e7a5184c5f1142fc511d80c6d5b5
patybn3 <paty.nigro@gmail.com>
2020-05-29 11:10:50 -0400

    Server with nodemon working

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const http = require('http')
+const reload = require('reload')
+const opn = require('opn')
+
+
+const app = express()
+
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+app.set('src', './src')
+app.use(express.static('src'))
+
+app.get('/', (req, res) => res.render('index'))
+
+const server = http.createServer(app)
+server.listen(8080, function() {
+  console.log('Listening to port 8080...')
+//   if (process.send) {
+//     process.send({ event:'online', url:'http://localhost:8080/' });
+// }
+  return opn('http://localhost:8080')
+})
+
+
+reload(app)
